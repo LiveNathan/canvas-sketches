749ef6f9c6937a1eecb74b28492ea54e3eee6f13
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-12 10:57:04 -0500

    Implement interactive drawing on canvas
    The main.js file has been updated to allow interactive drawing on a 300x300 canvas. This includes the addition of a fill rectangle method, a clear rectangle method, multiple stroke rectangle methods and a drawTriangle function that creates a triangle with a specified color. The ctx variable was also introduced to replace the context variable for readability.
    
    In the index.html file, width and height attributes were removed from the canvas element as they are now set in the main.js file. These changes were made to improve the user drawing experience.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import './index.css'
 
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext('2d');
+[ canvas.width, canvas.height ] = [300, 300];
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgb(200, 0, 0)' // red
+ctx.fillRect(10, 10, 300, 300); // starting x, starting y, width, height
+ctx.clearRect(50, 50, 50, 50);
+ctx.strokeRect(100, 100, 50, 50);
+ctx.strokeRect(110, 110, 50, 50);
+ctx.strokeRect(120, 120, 50, 50);
+
+ctx.strokeStyle = '#AE81DB'
+ctx.beginPath()
+ctx.moveTo(250,30)
+ctx.lineTo(50,250)
+ctx.stroke()
+
+function drawTriangle(ctx, x1, y1, x2, y2, x3, y3, color = 'black') {
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.closePath();
+    ctx.stroke();
+}
+drawTriangle(ctx, 50, 50, 100, 100, 50, 200);
