b063d9c52acbcdd6ebccf28a4b91c9db0c04ca44
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-13 09:19:32 -0500

    Add circle drawing function and Point class in main.js
    The changes in this commit include the addition of a function to draw circles and a Point class within main.js. The circle drawing function allows more diverse shapes to be drawn on the canvas. The addition of a new Point class provides an efficient way to handle points on the canvas by creating point objects and utilizing operation methods such as add. Also, minor syntax clean-up for better code readability is done as well.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 const canvas = document.getElementById("canvas");
-[ canvas.width, canvas.height ] = [300, 300];
+[canvas.width, canvas.height] = [300, 300];
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'rgb(200, 0, 0)' // red
@@ -13,8 +13,8 @@ ctx.strokeRect(120, 120, 50, 50);
 
 ctx.strokeStyle = '#AE81DB'
 ctx.beginPath()
-ctx.moveTo(250,30)
-ctx.lineTo(50,250)
+ctx.moveTo(250, 30)
+ctx.lineTo(50, 250)
 ctx.stroke()
 
 function drawTriangle(ctx, x1, y1, x2, y2, x3, y3, color = 'black') {
@@ -26,4 +26,30 @@ function drawTriangle(ctx, x1, y1, x2, y2, x3, y3, color = 'black') {
     ctx.closePath();
     ctx.stroke();
 }
+
 drawTriangle(ctx, 50, 50, 100, 100, 50, 200);
+
+function drawCircle(x, y, r, color = "black") {
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+drawCircle(150, 150, 50);
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(point) {
+        this.x += point.x
+        this.y += point.y
+    }
+}
+
+const origin = new Point(0, 0);
+const offset = new Point(10, 10);
+origin.add(offset);
